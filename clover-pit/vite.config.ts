6b8a35ba6da8107c52d58d0c95faba9e56663fa5
Daniil Bondarchuk <bondarchuk19988@gmail.com>
2026-03-20 10:27:18 +0700

    я сдохну

--- clover-pit/vite.config.ts
+++ clover-pit/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CloverPitCloneReact/', // ЗАМЕНИТЕ на название вашего репозитория!
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
